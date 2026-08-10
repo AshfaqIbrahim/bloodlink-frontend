26dbe8646b39e278393c16569359f20aaeada905
Ashfaq Ibrahim <ashfaqibrahim09@gmail.com>
2026-08-10 12:34:09 +0530

    Added User module Quick stats section

--- src/pages/user/Dashboard.jsx
+++ src/pages/user/Dashboard.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import WelcomeSection from "../../components/user/WelcomeSection";
+import QuickStats from "../../components/user/QuickStats";
 
 function Dashboard() {
   return (
     <div>
       <WelcomeSection />
+      <QuickStats />
     </div>
   );
 }
